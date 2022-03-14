a7d1db7ab0e4019f0b2b9deeddb91f7e2f8aecaf
profoundsea25 <profoundsea25@gmail.com>
2022-03-14 23:35:08 +0900

    7. 로그인 처리2 - 필터, 인터셉터 ~End, 2022.03.14

--- 6_login/src/main/java/hello/login/web/HomeController.java
+++ 6_login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,7 +28,7 @@ public class HomeController {
         return "home";
     }
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if (memberId == null) {
             return "home";
@@ -43,7 +44,7 @@ public class HomeController {
         return "loginHome";
     }
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         // 세션 관리자에 저장된 회원 정보 조회
@@ -57,7 +58,7 @@ public class HomeController {
         return "loginHome";
     }
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         // 세션이 없으면 home
@@ -73,14 +74,26 @@ public class HomeController {
         }
 
         // 세션이 유지되면 로그인된 홈으로 이동
-       model.addAttribute("member", loginMember);
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+//    @GetMapping("/")
+    public String homeLoginV3Spring(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
+            Model model) {
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 
     @GetMapping("/")
-    public String homeLoginV3Spring(
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
-            Model model) {
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
         // 세션에 회원 데이터가 없으면 home
         if (loginMember == null) {
             return "home";
