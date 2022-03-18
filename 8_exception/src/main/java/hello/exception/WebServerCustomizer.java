4aca7da914b3ae4134c9b66287b5732c0d2a2958
profoundsea25 <profoundsea25@gmail.com>
2022-03-19 00:50:06 +0900

    8. 예외 처리와 오류 페이지 ~End, 2022.03.18

--- 8_exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ 8_exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,8 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//스프링 부트가 제공하는 기본 오류 메커니즘을 사용하기 위해 주석처리
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
     // 서블릿 오류 페이지 등록
